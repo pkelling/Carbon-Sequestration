80399c5004b8d9f0874b5bc53b1c75e8ad92780c
PK <pkelling94@gmail.com>
2019-03-17 20:00:53 -0500

    Created regular matlab file.

---
+++ main.m
@@ -0,0 +1,18 @@
+clear all;
+close all;
+clc;
+
+
+%Load emmisions data
+[emmisionsRatesByState,labels] = xlsread("egrid2016_summarytables.xlsx",4);
+carbonEfficiencyByState = emmisionsRatesByState(1:end-1,1);
+states = labels(5:56,1);
+
+[stateResourceMix, labels] = xlsread("egrid2016_summarytables.xlsx",5);
+
+%list of power Sources
+powerSources = labels(3,4:14);
+
+%power sources percentages by state 
+sourcesByState = stateResourceMix(1:52,3:end);
+energyByState = stateResourceMix(1:52,2);
